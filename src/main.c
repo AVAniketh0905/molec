f44019c0ec2ce65f217e2c80ab04b98b7c6e5809
AVAniketh0905 <adimoolamaniketh@gmail.com>
2025-03-01 19:12:36 +0530

    hello triangle

--- src/main.c
+++ src/main.c
@@ -4,6 +4,20 @@
 #include <freetype2/ft2build.h>
 #include FT_FREETYPE_H
 
+const char *vertex_shader_src = "#version 330 core\n"
+                                "layout (location = 0) in vec3 aPos;\n"
+                                "void main()\n"
+                                "{\n"
+                                "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                "}\0";
+
+const char *fragment_shader_src = "#version 330 core\n"
+                                  "out vec4 FragColor;\n"
+                                  "void main()\n"
+                                  "{\n"
+                                  "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                  "}\0";
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -15,8 +29,20 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, GL_TRUE);
 };
 
+void ErrLog(GLuint program, GLenum pname, int success, char *infoLog)
+{
+    glGetProgramiv(program, pname, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+    }
+};
+
 int main()
 {
+    int success;
+    char infoLog[512];
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -39,12 +65,61 @@ int main()
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    float vertices[9] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f};
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int vertex_shader;
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int fragment_shader;
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glShaderSource(vertex_shader, 1, &vertex_shader_src, NULL);
+    glCompileShader(vertex_shader);
+    ErrLog(vertex_shader, GL_COMPILE_STATUS, success, infoLog);
+
+    glShaderSource(fragment_shader, 1, &fragment_shader_src, NULL);
+    glCompileShader(fragment_shader);
+    ErrLog(fragment_shader, GL_COMPILE_STATUS, success, infoLog);
+
+    unsigned int gl_shader_prg;
+    gl_shader_prg = glCreateProgram();
+
+    glAttachShader(gl_shader_prg, vertex_shader);
+    glAttachShader(gl_shader_prg, fragment_shader);
+    glLinkProgram(gl_shader_prg);
+
+    glLinkProgram(gl_shader_prg);
+    ErrLog(gl_shader_prg, GL_LINK_STATUS, success, infoLog);
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(gl_shader_prg);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwPollEvents();
         glfwSwapBuffers(window);

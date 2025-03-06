f21bb6d62037b58c3b6d3f1444a2f556e85c3559
AVAniketh0905 <adimoolamaniketh@gmail.com>
2025-03-06 10:39:03 +0530

    3d cube camera wasd

--- src/main.c
+++ src/main.c
@@ -7,6 +7,9 @@
 #include <freetype2/ft2build.h>
 #include FT_FREETYPE_H
 
+float deltaTime = 0.0f; // Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -15,7 +18,35 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
         glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+};
+
+void processCameraInput(GLFWwindow *window, vec3 cameraPos, vec3 cameraFront, vec3 cameraUp)
+{
+    const float cameraSpeed = 0.05f; // adjust accordingly
+    vec3 right;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        glm_vec3_muladds(cameraFront, cameraSpeed, cameraPos);
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        glm_vec3_muladds(cameraFront, -cameraSpeed, cameraPos);
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        glm_vec3_cross(cameraFront, cameraUp, right);
+        glm_vec3_normalize(right);
+        glm_vec3_muladds(right, -cameraSpeed, cameraPos);
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        glm_vec3_cross(cameraFront, cameraUp, right);
+        glm_vec3_normalize(right);
+        glm_vec3_muladds(right, cameraSpeed, cameraPos);
+    }
 };
 
 static void ErrLog(GLuint program, GLenum pname, int success, char *infoLog)
@@ -145,9 +176,14 @@ int main()
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0);
 
+    vec3 cameraPos = {0.0f, 0.0f, 3.0f};
+    vec3 cameraFront = {0.0f, 0.0f, -1.0f};
+    vec3 cameraUp = {0.0f, 1.0f, 0.0f};
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
+        processCameraInput(window, cameraPos, cameraFront, cameraUp);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -156,11 +192,15 @@ int main()
 
         mat4 model;
         glm_mat4_identity(model);
-        glm_rotate(model, (float)glfwGetTime(), (vec3){0.5f, 1.0f, 0.0f});
+        glm_rotate(model, glm_rad(55.0f), (vec3){0.0f, 1.0f, 0.0f});
 
         mat4 view;
         glm_mat4_identity(view);
-        glm_translate(view, (vec3){0.0f, 0.0f, -3.0f});
+
+        vec3 camTarget;
+        glm_vec3_add(cameraPos, cameraFront, camTarget);
+
+        glm_lookat(cameraPos, camTarget, cameraUp, view);
 
         mat4 projection;
         glm_perspective(glm_rad(45.0f), 800.0f / 600.0f, 0.1f, 100.0f, projection);

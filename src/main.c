86fae2558cb8b05ae974550af76b63ce9d88babb
AVAniketh0905 <adimoolamaniketh@gmail.com>
2025-03-20 15:47:26 +0530

    molecGLL

--- src/main.c
+++ src/main.c
@@ -93,7 +93,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "LearningGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "MolecGL", NULL, NULL);
     if (window == NULL)
     {
         printf("Failed to create GLFW window\n");
